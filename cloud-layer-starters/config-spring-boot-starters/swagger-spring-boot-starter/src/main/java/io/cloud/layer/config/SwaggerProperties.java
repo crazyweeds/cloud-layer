ab808cdc0e872c18aa49729925ec3ffc5f819cbb
crazyweeds <crazyweeds@gmail.com>
2019-02-27 23:21:35 +0800

    SwaggerProperties 自动配置文件模块增加注释

--- cloud-layer-starters/config-spring-boot-starters/swagger-spring-boot-starter/src/main/java/io/cloud/layer/config/SwaggerProperties.java
+++ cloud-layer-starters/config-spring-boot-starters/swagger-spring-boot-starter/src/main/java/io/cloud/layer/config/SwaggerProperties.java
@@ -12,12 +12,39 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("swagger.api.info")
 public class SwaggerProperties {
 
+    /**
+     * Api接口版本
+     */
     private String version = "V1.0";
+
+    /**
+     * title名称
+     */
     private String title = "swagger-spring-boot-starter";
+
+    /**
+     * 描述信息
+     */
     private String description = "Swagger-UI";
+
+    /**
+     * 团队URL
+     */
     private String termsOfServiceUrl = "http://www.cloud-layer.io";
+
+    /**
+     * 协议
+     */
     private String license = "Apache License";
+
+    /**
+     * 协议内容地址
+     */
     private String licenseUrl = "https://www.apache.org/licenses/LICENSE-2.0";
+
+    /**
+     * 联系人
+     */
     private String contactName = "RippleChan";
 
 
