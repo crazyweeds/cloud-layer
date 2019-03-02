22000a62f4824feff130a030201eca0b91541422
crazyweeds <crazyweeds@gmail.com>
2019-03-02 23:28:34 +0800

    module 名称调整

---
+++ cloud-layer-server/eureka-client/src/main/java/io/cloud/layer/client/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package io.cloud.layer.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author RippleChan
+ * @date 2019-03-02 23:14
+ */
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
