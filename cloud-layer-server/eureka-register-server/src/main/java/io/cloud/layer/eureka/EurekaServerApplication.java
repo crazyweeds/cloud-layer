629f0c21a27a76f6c3378b0346a4a425afa0a80d
crazyweeds <crazyweeds@gmail.com>
2019-03-02 21:16:50 +0800

    增加eureka-server模块

---
+++ cloud-layer-server/eureka-register-server/src/main/java/io/cloud/layer/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package io.cloud.layer.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author RippleChan
+ * @date 2019-03-02 18:57
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ cloud-layer-server/zuul-gateway-server/src/main/java/io/cloud/layer/zuul/ZuulServerApplication.java
@@ -0,0 +1,20 @@
+package io.cloud.layer.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+/**
+ * @author RippleChan
+ * @date 2019-03-02 03:28
+ */
+@SpringBootApplication
+//@EnableZuulProxy
+@EnableZuulServer
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+
+}
