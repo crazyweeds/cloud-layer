159d4e764887855753cffc1fa4578c64c3f76af4
crazyweeds <crazyweeds@gmail.com>
2019-03-08 00:10:34 +0800

    web socket config

--- cloud-layer-starters/config-spring-boot-starters/websocket-spring-boot-starter/src/main/java/io/cloud/layer/websocket/config/WebSocketConfig.java
+++ cloud-layer-starters/config-spring-boot-starters/websocket-spring-boot-starter/src/main/java/io/cloud/layer/websocket/config/WebSocketConfig.java
@@ -1,14 +1,16 @@
 package io.cloud.layer.websocket.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Component
+@Slf4j
 public class WebSocketConfig {
 
     {
-        System.out.println("WebSocketConfig injected to IOC...");
+        log.info("WebSocketConfig injected to IOC...");
     }
 
     @Bean
