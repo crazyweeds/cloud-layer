a6a7bfb83e42fce16b10e2c6b6910a4dd509e57f
crazyweeds <crazyweeds@gmail.com>
2019-03-10 10:21:27 +0800

    code generator

---
+++ cloud-layer-helper/cloud-layer-code-generate/src/main/java/io/cloud/layer/code/CodeApplication.java
@@ -0,0 +1,34 @@
+package io.cloud.layer.code;
+
+import io.cloud.layer.code.utils.DatasourceUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author RippleChan
+ * @date 2019-03-09 16:24
+ */
+public class CodeApplication {
+
+    public static void main(String[] args) {
+        DriverManagerDataSource dataSource = DatasourceUtils.getDataSource();
+        JdbcTemplate jdbcTemplate=new JdbcTemplate(dataSource);
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from COLUMNS;");
+        for (int i = 0; i < maps.size(); i++) {
+            Map<String, Object> map = maps.get(i);
+            Iterator<Map.Entry<String, Object>> iterator = map.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, Object> next = iterator.next();
+                String key = next.getKey();
+                Object value = next.getValue();
+                System.out.println(key + ":" + value);
+            }
+            System.out.println("------------------");
+        }
+    }
+
+}
