1714694710cc77c39467a2113afbfb58b5a8ab59
crazyweeds <crazyweeds@gmail.com>
2019-03-13 00:22:37 +0800

    功能接口定义

---
+++ cloud-layer-helper/cloud-layer-code-generate/src/main/java/io/cloud/layer/code/service/impl/TemplateServiceImpl.java
@@ -0,0 +1,49 @@
+package io.cloud.layer.code.service.impl;
+
+import io.cloud.layer.code.service.TemplateService;
+
+/**
+ * 🤭
+ *
+ * @author RippleChan
+ * @date 2019-03-11 23:19
+ */
+public class TemplateServiceImpl implements TemplateService {
+
+
+    @Override
+    public void pojo() {
+
+    }
+
+    @Override
+    public void service() {
+
+    }
+
+    @Override
+    public void serviceImpl() {
+
+    }
+
+    @Override
+    public void mapper() {
+
+    }
+
+    @Override
+    public void mapperXml() {
+
+    }
+
+    @Override
+    public void controller() {
+
+    }
+
+    @Override
+    public void feign() {
+
+    }
+
+}
