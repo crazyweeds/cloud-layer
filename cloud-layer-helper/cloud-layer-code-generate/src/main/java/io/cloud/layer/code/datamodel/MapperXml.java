685253537ac395d1cc1b2464d63ac93ad5088847
crazyweeds <crazyweeds@gmail.com>
2019-03-30 13:27:40 +0800

    Model

---
+++ cloud-layer-helper/cloud-layer-code-generate/src/main/java/io/cloud/layer/code/datamodel/MapperXml.java
@@ -0,0 +1,11 @@
+package io.cloud.layer.code.datamodel;
+
+/**
+ * @author RippleChan
+ * @date 2019-03-21 00:48
+ */
+public class MapperXml extends ServiceModel {
+
+    private static final long serialVersionUID = 6445480168344236929L;
+
+}
