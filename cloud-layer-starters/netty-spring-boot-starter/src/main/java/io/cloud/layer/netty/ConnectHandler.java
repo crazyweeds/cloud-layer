ed50e23267603064737925ee07ff2e4b7947d827
crazyweeds <crazyweeds@gmail.com>
2019-06-02 01:26:54 +0800

    other

---
+++ cloud-layer-starters/netty-spring-boot-starter/src/main/java/io/cloud/layer/netty/ConnectHandler.java
@@ -0,0 +1,13 @@
+package io.cloud.layer.netty;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author RippleChan
+ * @date 2019-05-20 23:47
+ */
+public class ConnectHandler extends ChannelInboundHandlerAdapter {
+
+
+
+}
--- cloud-layer-starters/tail-spring-boot-starter/src/main/java/io/cloud/layer/tail/TailApplication.java
+++ cloud-layer-starters/tail-spring-boot-starter/src/main/java/io/cloud/layer/tail/TailApplication.java
@@ -2,6 +2,7 @@ package io.cloud.layer.tail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 前期测试，starter化后删除
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019-03-07 00:19
  */
 @SpringBootApplication
+@RequestMapping
 public class TailApplication {
 
     public static void main(String[] args) {
